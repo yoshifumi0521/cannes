0fad9fa273440be43aedd204c4493b161a6f9382
yoshifumi0521 <yoshifumi0521@gmail.com>
2014-03-08 13:55:39 +0900

    修正

--- config/config.php
+++ config/config.php
@@ -1,5 +1,16 @@
 <?php
+// centos symfony directories
+$sf_symfony_lib_dir  = '/usr/lib/php/pear/symfony';
+$sf_symfony_data_dir = '/usr/lib/php/pear/data/symfony';
 
-// symfony directories
-$sf_symfony_lib_dir  = '/usr/share/php/symfony';
-$sf_symfony_data_dir = '/usr/share/php/data/symfony';
+if (!file_exists($sf_symfony_lib_dir)) {
+    // ubuntu symfony directories
+    $sf_symfony_lib_dir  = '/usr/share/php/symfony';
+    $sf_symfony_data_dir = '/usr/share/php/data/symfony';
+}
+
+if (!file_exists($sf_symfony_lib_dir)) {
+    // windows symfony directories(vertrigo)
+    $sf_symfony_lib_dir  = 'C:\VertrigoServ\Php\PEAR\symfony';
+    $sf_symfony_data_dir = 'C:\VertrigoServ\Php\data\symfony';
+}
