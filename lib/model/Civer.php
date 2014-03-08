c08569cff00de25e6b5718bc10cacd8a15144766
yoshifumi0521 <yoshifumi0521@gmail.com>
2014-03-08 14:45:45 +0900

    データベース修正

---
+++ lib/model/Civer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'civer' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Civer extends BaseCiver
+{
+}
---
+++ lib/model/CiverPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'civer' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class CiverPeer extends BaseCiverPeer
+{
+}
