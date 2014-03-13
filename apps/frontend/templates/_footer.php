591b1ba3ef10ba8356bc64f9827f4cf684a1a8e5
devuser <devuser@www5290uo.sakura.ne.jp>
2014-03-13 11:06:23 +0900

    footerをいれる

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,16 +1,20 @@
 <?php $custom = Spyc::YAMLLoad(SF_ROOT_DIR.'/config/custom.yml'); ?>
+<script type="text/javascript">
 
-<!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount','<?php echo $custom["core"]["ga_id"]; ?>']);
+  _gaq.push(['_setDomainName', '<?php echo $custom["core"]["domain"]; ?>']);
+  _gaq.push(['_trackPageview']);
 
-  ga('create', '<?php echo $custom["core"]["ga_id"]; ?>', '<?php echo $custom["core"]["domain"]; ?>');
-  ga('send', 'pageview');
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
 </script>
+
+
 
 <footer>
     <div class="container">
